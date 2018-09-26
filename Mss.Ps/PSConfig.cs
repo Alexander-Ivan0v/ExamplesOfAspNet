2643f729e5ba2f17870fbfbc18979b6d789e8fb0
Alexander-Ivan0v <43615587+Alexander-Ivan0v@users.noreply.github.com>
2018-09-26 22:30:09 +0300

    Forth commit

---
+++ Mss.Ps/PSConfig.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+
+namespace Mss.Ps
+{
+    public class PSConfig
+    {
+        private static string _profile;
+        private static Runspace _rs;
+
+        public static Runspace GetPSConfig { get { return rs; } }
+
+        public static string Profile
+        {
+            get
+            {
+                return _profile;
+            }
+            set
+            {
+                _profile =  value;
+                AddVariable( "Profile", _profile );
+                PS.ExecutePS(_profile);
+            }
+        }
+
+        public static Runspace rs
+        {
+            get
+            {
+                if (_rs == null)
+                {
+                    _rs = RunspaceFactory.CreateRunspace();
+                    _rs.ThreadOptions = PSThreadOptions.Default;
+                    _rs.Open();
+
+                    return _rs;
+                }
+                return _rs;
+            }
+        }
+
+        public static void AddVariable(string name, object value)
+        {
+            rs.SessionStateProxy.SetVariable(name, value);
+        }
+    }
+}
---
+++ Mss.Ps/Powershell.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.ObjectModel;
+using System.Management.Automation;
+
+namespace Mss.Ps
+{
+    public class Powershell : IPowershell
+    {
+        // --------- Pulished variables for PowerShell -------------
+        string _testVar = "Big Ass";
+        public string TestVar { get { return _testVar; } set { _testVar = value; PSConfig.AddVariable("TestVar", TestVar); } }
+        // ---------------------------------------------------------
+
+        public Powershell()
+        {
+            _testVar = "Test";
+        }
+
+        public object GetVariable(string name)
+        {
+            return PSConfig.rs.SessionStateProxy.PSVariable.GetValue(name);
+        }
+
+        public void SetVariable(string name, object value)
+        {
+            PSConfig.AddVariable(name, value);
+        }
+
+        public PSObject Execute(string script)
+        {
+            return PS.ExecutePS(script);            
+        }
+    }
+}
